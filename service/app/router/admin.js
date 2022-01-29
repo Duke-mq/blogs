774a30ca0d657bac3d2f275820310fa7feaa8ec4
许绵渠 <294848617@qq.com>
2022-01-29 15:40:23 +0800

    暂时不加入路由守卫

--- service/app/router/admin.js
+++ service/app/router/admin.js
@@ -4,5 +4,6 @@ module.exports = app => {
     var adminauth = app.middleware.adminauth()
     router.get('/admin/index',controller.admin.main.index)
     router.post('/admin/checkLogin',controller.admin.main.checkLogin)
-    router.get('/admin/getTypeInfo',adminauth,controller.admin.main.getTypeInfo)
+    // router.get('/admin/getTypeInfo',adminauth,controller.admin.main.getTypeInfo)
+    router.get('/admin/getTypeInfo',controller.admin.main.getTypeInfo)
 }
