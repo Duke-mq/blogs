b825a140828a196a3b24d7a05d45a94091a21591
许绵渠 <294848617@qq.com>
2022-01-29 16:23:56 +0800

    完善添加文章事件处理

--- admin/src/pages/AddArticle.js
+++ admin/src/pages/AddArticle.js
@@ -9,9 +9,6 @@ import axios from 'axios' //引入后台获得数据的方法
 import servicePath from '../config/apiUrl'
 const { Option } = Select // 下拉列表中的每一项，这个需要解析出来
 const { TextArea } = Input // 多行文本框，这个是在Input里面的，所以注意引入的方式
-
-
-
 function AddArticle(props){
     const [articleId,setArticleId] = useState(0)  // 文章的ID，如果是0说明是新增加，如果不是0，说明是修改
     const [articleTitle,setArticleTitle] = useState('')   //文章标题
@@ -34,9 +31,9 @@ function AddArticle(props){
         smartLists: true,
         smartypants: false,
     });
-
-
-
+    const selectTypeHandle = value =>{
+        setSelectType(value)
+    }
     const getTypeInfo =()=>{
         axios({
             method:'get',
@@ -62,7 +59,7 @@ function AddArticle(props){
     useEffect(()=> {
         getTypeInfo()
     },[])
-
+    
     //从中台得到文章类别信息
     const changeContent = (e)=>{
         /*把文章的值赋为输入内容*/
@@ -76,20 +73,44 @@ function AddArticle(props){
         setIntroducehtml(html)
     }
 
+    const saveArticle = ()=>{
+        if(selectedType === '请选择类型'){
+            message.error('必须选择文章类别')
+            return false
+        }else if(!articleTitle){
+            message.error('文章名称不能为空')
+            return false
+        }else if(!articleContent){
+            message.error('文章内容不能为空')
+            return false
+        }else if(!introducemd){
+            message.error('简介不能为空')
+            return false
+        }else if(!showDate){
+            message.error('发布日期不能为空')
+            return false
+        }
+        message.success('检验通过')
+    }
+
     return (
         <div style={{height:"100%"}}>
             <Row gutter={5}>
                 <Col span={18}>
                     <Row gutter={10} >
                         <Col span={20}>
-                            <Input placeholder="博客标题" size="large" />
+                            <Input
+                                placeholder="博客标题"
+                                size="large"
+                                onChange={e =>
+                                    setArticleTitle(e.target.value)
+                                }/>
                         </Col>
                         <Col span={4}>
                             &nbsp;
-                            <Select defaultValue={selectedType} size="large" >
+                            <Select defaultValue={selectedType} size="large" onSelect={selectTypeHandle} >
                                 {
                                     typeInfo.map((item,index)=> {
-                                            console.log(typeInfo)
                                             return(<Option key={index} value={item.id}> {item.typeName}</Option>
                                             )})
                                 }
@@ -117,7 +138,13 @@ function AddArticle(props){
                     <Row>
                         <Col span={24}>
                             <Button  size="large">暂存文章</Button>&nbsp;
-                            <Button type="primary" size="large">发布文章</Button>
+                            <Button type="primary"
+                                    size="large"
+                                    onClick={
+                                        saveArticle
+                                    }
+
+                            >发布文章</Button>
                         </Col>
                         <Col span={24}>
                             <br/>
@@ -135,7 +162,11 @@ function AddArticle(props){
                         </Col>
                         <Col span={12}>
                             <div className="date-select">
-                                <DatePicker placeholder="发布日期" size="large"/>
+                                <DatePicker
+                                    placeholder="发布日期"
+                                    size="large"
+                                    onChange={(date,dateString) => setShowDate(dateString)}
+                                />
                             </div>
                         </Col>
                     </Row>
